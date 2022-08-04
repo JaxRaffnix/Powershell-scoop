6a06e1e6a0446617ca8d80ba8e8bd5954ca4eff3
Jax <75493600+JaxRaffnix@users.noreply.github.com>
2022-08-04 18:25:34 +0200

    added installer file

---
+++ scoop apps.ps1
@@ -0,0 +1,27 @@
+scoop bucket add extras
+
+scoop install googlechrome
+
+scopp install vscode
+
+C:\Users\janho\scoop\apps\vscode\current\install-context.reg
+
+scopp install keepass
+
+scopp install aimp
+
+scopp install windows-terminal
+
+scopp install python
+
+scopp install latex
+
+scoop install whatsapp
+
+scoop bucket add spotify https://github.com/TheRandomLabs/Scoop-Spotify.git
+
+scoop install spotify-latest
+
+scoop install thunderbird
+
+scoop install git
