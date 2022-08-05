df6b8c1672999734469906a5b71ce12bf6e90bd8
JaxRaffnix <75493600+JaxRaffnix@users.noreply.github.com>
2022-08-05 12:04:50 +0200

    added software

--- scoop apps.ps1
+++ scoop apps.ps1
@@ -1,27 +1,49 @@
-scoop bucket add extras
 
-scoop install googlechrome
 
-scopp install vscode
+
+
 
 C:\Users\janho\scoop\apps\vscode\current\install-context.reg
 
-scopp install keepass
+# Include buckets
+scoop bucket add extras
+scoop bucket add nonportable
 
-scopp install aimp
-
+# Coding
+scoop install vscode
 scopp install windows-terminal
 
-scopp install python
-
-scopp install latex
-
-scoop install whatsapp
-
-scoop bucket add spotify https://github.com/TheRandomLabs/Scoop-Spotify.git
-
-scoop install spotify-latest
-
-scoop install thunderbird
-
+# helpers
 scoop install git
+scopp install keepass
+scoop install 7zip
+scoop install treesize-free
+scopp install aimp
+scoop install nvidia-display-driver-np
+scoop install restic
+
+# Languages
+scopp install python
+scopp install miktex
+
+# Communication
+scoop install thunderbird
+scoop install whatsapp
+scoop install signal
+scoop install microsoft-teams
+scoop install zoom
+scoop install discord
+
+# Office
+scoop install googlechrome
+scoop install office-365-apps-minimal-np
+scoop install nextcloud
+scoop install spotify
+
+# Games
+scoop install playnite
+scoop install steam
+scoop install ubisoftconnect
+
+# Missing
+# box, epic games, onenote, pdf24, roccat swarm, 
